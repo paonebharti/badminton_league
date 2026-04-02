226b8b8757dd90011b0de043d3ba579a4264e90b
P-one-1 <bpawan96@gmail.com>
2026-04-02 11:02:57 +0000

    Modify readme file for reference

---
+++ db/migrate/20260401111254_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayerss < ActiveRecord::Migration[8.1]
+  def change
+    create_table :players do |t|
+      t.string :name, null: false
+      t.integer :age, null: false
+      t.string :email, null: false
+      t.string :phone, null: false
+      t.boolean :active, null: false, default: true
+
+      t.timestamps
+    end
+  end
+end
